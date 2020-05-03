668b1ca09b6f108aa0a6fece3817bcd9d03dc2ef
Nicole Tilbe <nicolecali2406@outlook.com>
2020-05-02 21:51:27 -0400

    initial push

---
+++ Simple Snake Game/Simple Snake Game/Simple Snake Game.cpp
@@ -0,0 +1,35 @@
+// Simple Snake Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void Setup()
+{
+
+
+}
+
+void Draw()
+{
+
+
+}
+
+void Input()
+{
+
+
+}
+
+void Logic()
+{
+
+
+}
+
+int main()
+{
+    return 0;
+}
