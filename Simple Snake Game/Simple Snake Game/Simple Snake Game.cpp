dd7f6bb1dd6c167323f2b1f6a990b6cefbc360a0
Nicole Tilbe <nicolecali2406@outlook.com>
2020-05-02 22:33:05 -0400

    created game board

--- Simple Snake Game/Simple Snake Game/Simple Snake Game.cpp
+++ Simple Snake Game/Simple Snake Game/Simple Snake Game.cpp
@@ -5,16 +5,50 @@
 
 using namespace std;
 
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection{STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
 void Setup()
 {
-
-
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
 }
 
 void Draw()
 {
+    system("CLS");
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
 
+    cout << endl;
 
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0)
+                cout << "#";
+            else
+                cout << " ";
+
+			if (j == width - 1)
+				cout << "#";
+        }
+        cout << endl;
+    }
+
+	for (int i = 0; i < width + 2; i++)
+		cout << "#";
+    cout << endl;
 }
 
 void Input()
@@ -31,5 +65,14 @@ void Logic()
 
 int main()
 {
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+        //Sleep(10); - this sleep function can slow the game speed; 10 represents 10 ms
+    }
+
     return 0;
 }
