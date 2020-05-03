4d4f4f643fd2a5dd185b7e830df7e35862e7d5aa
Nicole Tilbe <nicolecali2406@outlook.com>
2020-05-03 14:36:13 -0400

    completed game

--- Simple Snake Game/Simple Snake Game/Simple Snake Game.cpp
+++ Simple Snake Game/Simple Snake Game/Simple Snake Game.cpp
@@ -2,6 +2,8 @@
 //
 
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
 
 using namespace std;
 
@@ -9,6 +11,8 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail; //specifies the length of the tail
 enum eDirection{STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
@@ -37,8 +41,25 @@ void Draw()
         {
             if (j == 0)
                 cout << "#";
+          
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
             else
-                cout << " ";
+            {
+                bool print = false;
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "o";
+                        print = true; 
+                    }
+                }
+				if (!print)
+					cout << " ";
+            }      
 
 			if (j == width - 1)
 				cout << "#";
@@ -49,18 +70,97 @@ void Draw()
 	for (int i = 0; i < width + 2; i++)
 		cout << "#";
     cout << endl;
+    cout << "Score: " << score << endl;
 }
 
+// using W, A, S, D keys to move the snake
 void Input()
 {
-
+    if (_kbhit()) //function to check if keyboard button was pressed
+    {
+        switch (_getch()) // will return the ASCII value of the character that was pressed
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+		case 'd':
+			dir = RIGHT;
+			break;
+		case 'w':
+			dir = UP;
+			break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
 
 }
 
 void Logic()
 {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
 
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
 
+    switch(dir)
+	{
+    case LEFT:
+        x--;
+		break;
+    case RIGHT:
+        x++;
+		break;
+    case UP:
+        y--;
+		break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+	}
+
+    //if (x > width || x < 0 || y > height || y < 0) //if the snake goes off the screen, game terminates
+    //    gameOver = true;
+
+    if (x >= width) 
+        x = 0; 
+    else if (x < 0)
+        x = width - 1;
+
+	if (y >= height)
+		y = 0;
+	else if (y < 0)
+		y = height - 1;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        // places fruit at a random spot in the game board
+		fruitX = rand() % width; 
+		fruitY = rand() % height;
+        nTail++;
+    }
 }
 
 int main()
@@ -71,7 +171,7 @@ int main()
         Draw();
         Input();
         Logic();
-        //Sleep(10); - this sleep function can slow the game speed; 10 represents 10 ms
+        Sleep(10); // this sleep function can slow the game speed; 10 represents 10 ms
     }
 
     return 0;
